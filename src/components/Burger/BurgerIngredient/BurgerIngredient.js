4fab73f03342c8d8cf3e6b2e7287f1c94c23a19f
Dieter Doman <dieter@jemstep.com>
2019-08-01 11:02:52 +0200

    Added proptype to burger ingrediant.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from 'BurgerIngredient.module.css';
+import PropTypes from 'prop-types';
 
 const BurgerIngredient = (props) => {
     let ingredient = null;
@@ -32,6 +33,10 @@ const BurgerIngredient = (props) => {
             ingredient = null;
     }
     return ingredient;
+};
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 };
 
 export default BurgerIngredient;
