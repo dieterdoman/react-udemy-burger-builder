9cd98e9aed913106b2c8ff7951382fd79a502629
Dieter Doman <dieter@jemstep.com>
2019-08-02 15:26:47 +0200

    Added error handling for get request.

--- src/containters/BurgerBuilder/BurgerBuilder.js
+++ src/containters/BurgerBuilder/BurgerBuilder.js
@@ -17,16 +17,20 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients: null,
         totalPrice: 4,
         purchaseable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
+    };
+
+    componentDidMount() {
+        axious.get('https://react-udemy-burger-build-5cf81.firebaseio.com/ingredients.json').then(response => {
+            this.setState({ingredients: response.data});
+        }).catch(error => {
+            this.setState({error: true});
+        });
     };
 
     addIngredientHandler = (type) => {
@@ -111,10 +115,26 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
 
-        let orderSummary = <OrderSummary ingredients={this.state.ingredients}
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner/>;
+
+        if (this.state.ingredients) {
+            orderSummary = <OrderSummary ingredients={this.state.ingredients}
                                          purchaseCanceled={this.purchaseCancelHandler}
                                          purchaseContinue={this.purchaseContinueHandler}
                                          price={this.state.totalPrice}/>;
+            burger = (
+                <Auxiliary>
+                    <Burger ingredients={this.state.ingredients}/>
+                    <BuildControls ingredientAdded={this.addIngredientHandler}
+                                   ingredientRemoved={this.removeIngredientHandler}
+                                   disabled={disabledInfo}
+                                   price={this.state.totalPrice}
+                                   purchaseable={this.state.purchaseable}
+                                   ordered={this.purchaseHandler}/>
+                </Auxiliary>
+            );
+        }
 
         if (this.state.loading) {
             orderSummary = <Spinner/>;
@@ -125,13 +145,7 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls ingredientAdded={this.addIngredientHandler}
-                               ingredientRemoved={this.removeIngredientHandler}
-                               disabled={disabledInfo}
-                               price={this.state.totalPrice}
-                               purchaseable={this.state.purchaseable}
-                               ordered={this.purchaseHandler}/>
+                {burger}
             </Auxiliary>
         );
     }
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,18 +4,18 @@ import Modal from "../../components/ui/Modal/Modal";
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-        state = {
-            error: null
-        };
-
-        componentDidMount() {
+        constructor(props) {
+            super(props);
+            this.state = {
+                error: null
+            };
             axios.interceptors.request.use(req => {
-               this.setState({error: null});
-               return req;
+                this.setState({error: null});
+                return req;
             });
             axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
-            })
+            });
         }
 
         errorConfirmedHandler = () => {
