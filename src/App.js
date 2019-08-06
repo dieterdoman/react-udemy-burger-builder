235ac0a0b5a54ca895483522ed125438c91e9e69
Dieter Doman <dieter@jemstep.com>
2019-08-06 16:19:32 +0200

    Added checkout summary component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containters/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containters/Checkout/Checkout";
 
 function App() {
   return (
     <div>
         <Layout>
             <BurgerBuilder />
+            <Checkout />
         </Layout>
     </div>
   );
