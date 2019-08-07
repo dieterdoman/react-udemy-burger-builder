8127886cfc580cf838b8911e460e67dc6f48ba28
Dieter Doman <dieter@jemstep.com>
2019-08-07 13:08:10 +0200

    Added custom input.

--- src/containters/Checkout/ContactData/ContactData.js
+++ src/containters/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/ui/Button/Button";
 import classes from './ContactData.module.css';
 import axious from '../../../axious-orders';
 import Spinner from "../../../components/ui/Spinner/Spinner";
+import Input from "../../../components/ui/Input/Input";
 
 class ContactData extends Component {
     state = {
@@ -43,10 +44,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name"/>
-                <input className={classes.Input} type="email" name="email" placeholder="Your email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal code"/>
+                <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal code"/>
                 <Button buttonType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
