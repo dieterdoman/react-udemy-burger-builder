f2cc84a8a0fdfe10956dffd5887446408080099f
Dieter Doman <dieter@jemstep.com>
2019-08-13 15:07:45 +0200

    Made orders use redux and async calls.

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -37,3 +37,42 @@ export const purchaseBurger = (orderData) => {
         });
     };
 };
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+};
+
+export const fetchOrdersFailed = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAILED,
+        error: error
+    }
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            }).catch(error => {
+            dispatch(fetchOrdersFailed(error))
+        });
+    };
+};
