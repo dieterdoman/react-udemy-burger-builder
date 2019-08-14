905ccf28ae12aa1d60a61d714e5e4038ebfb63bd
Dieter Doman <dieter@jemstep.com>
2019-08-14 14:09:20 +0200

    added environment variable to project.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/orders';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
