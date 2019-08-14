b1f488925a664b03a30ed39a4af8b65711bf8e1b
Dieter Doman <dieter@jemstep.com>
2019-08-14 14:05:23 +0200

    Refactored check validity in to utility.

--- src/containters/Checkout/ContactData/ContactData.js
+++ src/containters/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from "../../../components/ui/Input/Input";
 import {connect} from 'react-redux';
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import * as actionTypes from '../../../store/actions/index';
-import {updateObject} from "../../../shared/utility";
+import {updateObject, checkValidity} from "../../../shared/utility";
 
 class ContactData extends Component {
     state = {
@@ -116,24 +116,10 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     };
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-        if (rules.maxLength) {
-            isValid = value.length >= rules.maxLength && isValid;
-        }
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputId) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputId], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[inputId].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputId].validation),
             touch: true
         });
         const updatedOrderForm = updateObject(this.state.orderForm, {
