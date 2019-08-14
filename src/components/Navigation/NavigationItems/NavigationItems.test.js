046cc59b19cc811fea598a0a4a4401080ae13320
Dieter Doman <dieter@jemstep.com>
2019-08-14 15:09:38 +0200

    Added more tests for navigation items.

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,17 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({adapter: new Adapter()});
 
 describe('Navigation Items', () => {
+    let wrapper;
+    beforeEach(() => {
+         wrapper = shallow(<NavigationItems/>)
+    });
+
     it('should render 2 navigation item elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems isAuthenticated={false}/>);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 navigation item elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
