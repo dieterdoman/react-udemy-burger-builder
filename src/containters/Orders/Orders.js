020d2ffc3e2fa1f5a8c623b74c9e6fb3a75408fe
Dieter Doman <dieter@jemstep.com>
2019-08-22 16:34:17 +0200

    Converted checkout and order to functional components.

--- src/containters/Orders/Orders.js
+++ src/containters/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Order from "../../components/Order/Order";
 import axios from '../../axious-orders';
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
@@ -6,25 +6,23 @@ import * as actions from '../../store/actions/index';
 import {connect} from 'react-redux';
 import Spinner from "../../components/ui/Spinner/Spinner";
 
-class Orders extends Component {
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token, this.props.userId);
-    }
+const Orders = props => {
+    useEffect( () => {
+        props.onFetchOrders(props.token, props.userId);
+    }, []);
 
-    render() {
-        let orders = <Spinner/>;
-        if (!this.props.loading) {
-            orders = this.props.orders.map(order => (
-                <Order key={order.id} ingredients={order.ingredients} price={order.price}/>
-            ));
-        }
-        return (
-            <div>
-                {orders}
-            </div>
-        );
+    let orders = <Spinner/>;
+    if (!props.loading) {
+        orders = props.orders.map(order => (
+            <Order key={order.id} ingredients={order.ingredients} price={order.price}/>
+        ));
     }
-}
+    return (
+        <div>
+            {orders}
+        </div>
+    );
+};
 
 const mapStateToProps = state => {
     return {
