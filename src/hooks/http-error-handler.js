ffa6a195c7da2e22d03bfd64bd2212f1bfca0024
Dieter Doman <dieter@jemstep.com>
2019-08-23 10:02:11 +0200

    Added custom hook for error handling.

---
+++ src/hooks/http-error-handler.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react';
+
+export default httpclient => {
+    const [error, setError] = useState(null);
+    const reqInterceptor = httpclient.interceptors.request.use(req => {
+        setError(null);
+        return req;
+    });
+    const resInterceptor = httpclient.interceptors.response.use(res => res, err => {
+        setError(err);
+    });
+
+    useEffect(() => {
+        return () => {
+            httpclient.interceptors.request.eject(reqInterceptor);
+            httpclient.interceptors.response.eject(resInterceptor);
+        };
+    }, [reqInterceptor, resInterceptor]);
+
+    const errorConfirmedHandler = () => {
+        setError(null);
+    };
+
+    return [error, errorConfirmedHandler]
+};
